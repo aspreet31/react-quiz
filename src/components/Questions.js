a8f52fe28c5afb17cda7a5664442ca23f3b726cd
Aspreet kaur <103019348+aspreet31@users.noreply.github.com>
2024-08-18 14:30:25 +0530

    Update Questions.js

--- src/components/Questions.js
+++ src/components/Questions.js
@@ -3,7 +3,6 @@ import { useQuiz } from "../context/QuizContext";
 export  default function Questions(){
     const {questions,index}=useQuiz();
     const question = questions[index]
-    console.log(question)
     return <div>
         <h4>{question.question}</h4>
         <Options question={question}/>
